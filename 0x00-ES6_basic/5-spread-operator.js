86aa9e6b64702880d74f146e15ff69f5d482d214
Usman Adepoju <adepojuusman465@gmail.com>
2023-09-25 17:26:51 +0100

    Add solution to task-5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,11 +1,3 @@
-function concatArrays(...args) {
-    const array = [];
-    for (let arg of args) {
-        for (let i of arg) {
-            array.push(i);
-        }
-    }
-    return array;
+function concatArrays(array1, array2, string) {
+    return [...array1, ...array2, ...string];
 }
-
-console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
