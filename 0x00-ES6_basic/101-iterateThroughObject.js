3ed469704672f5170408fa64e67f8d588c01f932
Usman Adepoju <adepojuusman465@gmail.com>
2023-09-25 22:45:26 +0100

    Add solution to task-101

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const result = [];
+
+  for (const employee of reportWithIterator) {
+    result.push(employee);
+  }
+
+  return result.join(' | ');
+}
