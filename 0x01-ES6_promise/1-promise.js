9b1c1fe61be90e0a422951cde67557b42a14859b
Usman Adepoju <adepojuusman465@gmail.com>
2023-09-27 20:39:51 +0100

    Add solution to task-1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -6,7 +6,7 @@ export default function getFullResponseFromAPI(success) {
         body: 'Succes',
       });
     } else {
-      reject('The fake API is not working currently');
+      reject( new Error('The fake API is not working currently'));
     }
   });
 }
