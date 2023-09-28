f92d3c105ec1ebd73e790f6092c8648eaca1ff51
Usman Adepoju <adepojuusman465@gmail.com>
2023-09-28 04:38:24 +0100

    Add solution to task-3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
     .then((response) => {
       console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
     })
-    .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
