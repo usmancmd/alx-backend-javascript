cb5876c85e3b169c121768a9de5668b4fc536b89
Usman Adepoju <adepojuusman465@gmail.com>
2023-09-29 10:00:40 +0100

    Add solution to task-1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const objs = [19, 20, 34];
   return objs.map((obj) => new ClassRoom(obj));
 }
-
