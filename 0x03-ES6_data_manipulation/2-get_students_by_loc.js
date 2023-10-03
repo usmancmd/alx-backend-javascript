1d24a191a16361a42f03834949352535a68565e3
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-03 15:15:04 +0100

    Add solution to task-2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,6 @@
 export default function getStudentsByLocation(students, location) {
   const studentsByLocation = students.filter((student) => {
-    const studentObj = JSON.parse(JSON.stringify(student));
-    return studentObj.location === location ? studentObj : '';
+    return student.location === location ? student : '';
   });
   return studentsByLocation;
 }
