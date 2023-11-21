9b760f1f028127ef0bbdb7217da3b2056b25d051
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-21 17:35:42 +0100

    Add solution to task-3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+function countStudents (path) {
+  const students = {};
+  const fields = {};
+  let fileLength = 0;
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (error, content) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = content.toString().split('\r\n');
+
+        for (let i = 0; i < lines.length; i++) {
+          if (lines[i].trim() !== '') {
+            fileLength += 1;
+            const field = lines[i].split(',');
+
+            if (students[field[3]]) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+
+            if (fields[field[3]]) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const numberOfStudents = fileLength - 1;
+        console.log(`Number of students: ${numberOfStudents}`);
+        for (const key of Object.keys(fields)) {
+          if (key !== 'field') {
+            console.log(`Number of students in ${key}: ${fields[key]}. List: ${students[key].join(', ')}`);
+          }
+        }
+        resolve(content);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
