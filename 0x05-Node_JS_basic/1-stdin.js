1c183a50fe8740f1bd02e9692a6784aadbd99810
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-21 10:13:47 +0100

    Add solution to task-1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', function () {
+  const name = process.stdin.read();
+  if (name) { process.stdout.write(`Your name is: ${name}`); }
+});
+
+process.stdin.on('end', function () {
+  process.stdout.write('This important software is now closing\n');
+});
