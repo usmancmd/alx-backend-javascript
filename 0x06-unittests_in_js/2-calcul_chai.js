12a82c54fd803d1d4bdb967ab14a2cfbee6a1ee9
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-29 10:11:33 +0100

    Add test for type calculations using expect from chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,12 @@
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM')
+    return Math.round(a) + Math.round(b)
+  else if (type === 'SUBTRACT')
+    return Math.round(a) - Math.round(b)
+  else if (type === 'DIVIDE' && Math.round(b) === 0) {
+    return 'Error'
+  } else 
+     return Math.round(a) / Math.round(b)
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,38 @@
+const expect = require('chai').expect
+const calculateNumber = require('./1-calcul')
+
+describe('#calculateNumber() with type ADD', () => {
+  it('should sum 2.5 and 5.8', () => {
+    expect(calculateNumber('SUM', 2.5, 5.8)).to.equal(9)
+  })
+
+  it('should sum 2.2 and 5.3', () => {
+    expect(calculateNumber('SUM', 2.2, 5.3)).to.equal(7)
+  })
+
+  it('should sum 2 and 3.3', () => {
+    expect(calculateNumber('SUM', 2, 3.3)).to.equal(5)
+  })
+})
+
+describe('#calculateNumber() with type SUBTRACT', () => {
+  it('should round up and subtract if type is SUBTRACT', () => {
+    expect(calculateNumber('SUBTRACT', 2.2, 5.3)).to.equal(-3)
+    expect(calculateNumber('SUBTRACT', 2.1, 5.4)).to.equal(-3)
+    expect(calculateNumber('SUBTRACT', 7.5, 5.3)).to.equal(3)
+  })
+})
+
+describe('#calculateNumber() with type DIVIDE', () => {
+  it('should round up and divide if type is DIVIDE', () => {
+    expect(calculateNumber('DIVIDE', 4.2, 2.2)).to.equal(2)
+    expect(calculateNumber('DIVIDE', 8.5, 3.3)).to.equal(3)
+    expect(calculateNumber('DIVIDE', 11.5, 2.3)).to.equal(6)
+  })
+
+  it('should return ERROR string if round up b is 0', () => {
+    expect(calculateNumber('DIVIDE', 2.2, 0)).to.equal('Error')
+    expect(calculateNumber('DIVIDE', 5.8, 0)).to.equal('Error')
+    expect(calculateNumber('DIVIDE', 6, 0)).to.equal('Error')
+  })
+})
